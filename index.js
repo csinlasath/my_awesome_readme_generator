dd236fda24f88e236c3afc437838a08282bc99b1
Christopher Sinlasath <csinlasath@live.com>
2022-08-09 22:12:12 -0500

    Broke up existing methods into modules

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-// TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const questions = require("./utils/questions");
+const { writeFile } = require("./utils/generateFile");
 
 // TODO: Create a function to initialize app
 function init() {
-    const test = "test";
-    console.error("Hello");
+  console.error(questions);
+  writeFile();
 }
 
 // Function call to initialize app
